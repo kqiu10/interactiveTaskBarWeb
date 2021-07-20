d348153b82e08cd6ebb21c951c434cab72b7e4d9
kqiu10 <qiukan97@gmail.com>
2021-07-19 23:37:03 -0400

    added css for expenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
   return (
-    <div>
-      <h2>Hello there!</h2>
+    <div className='expense-item'>
+      <div>March 28th 2021</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$294.67</div>
+      </div>
     </div>
   )
 }
